f6d991b13d16a0d48206259317b13f22799f0480
Rafal Barszczewski <rafalb>
2013-02-24 22:34:28 +0100

    Added web presentation project (MVC 4)

---
+++ Source/EuroManager.WorldSimulator.Presentation.Web/App_Start/AuthConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EuroManager.WorldSimulator.Presentation.Web.Models;
+using Microsoft.Web.WebPages.OAuth;
+
+namespace EuroManager.WorldSimulator.Presentation.Web
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+        }
+    }
+}
