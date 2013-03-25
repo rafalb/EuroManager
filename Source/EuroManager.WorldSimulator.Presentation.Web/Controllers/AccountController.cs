2bbc18b07e3eb2b4181a8a4175352ee1817e2606
Rafal Barszczewski <rafalb>
2013-03-25 20:37:14 +0100

    Revert "Removed DotNetOpenAuth dependency"
    This reverts commit 06568fe83dbc5b1794dae67f801cc0945c218da2.

--- Source/EuroManager.WorldSimulator.Presentation.Web/Controllers/AccountController.cs
+++ Source/EuroManager.WorldSimulator.Presentation.Web/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Transactions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using DotNetOpenAuth.AspNet;
 using EuroManager.WorldSimulator.Presentation.Web.Filters;
 using EuroManager.WorldSimulator.Presentation.Web.Models;
 using Microsoft.Web.WebPages.OAuth;
@@ -190,126 +191,126 @@ namespace EuroManager.WorldSimulator.Presentation.Web.Controllers
             return View(model);
         }
 
-        //// POST: /Account/ExternalLogin
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult ExternalLogin(string provider, string returnUrl)
-        //{
-        //    return new ExternalLoginResult(provider, Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
-        //}
+        // POST: /Account/ExternalLogin
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            return new ExternalLoginResult(provider, Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
+        }
 
-        //// GET: /Account/ExternalLoginCallback
-        //[AllowAnonymous]
-        //public ActionResult ExternalLoginCallback(string returnUrl)
-        //{
-        //    AuthenticationResult result = OAuthWebSecurity.VerifyAuthentication(Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
-        //    if (!result.IsSuccessful)
-        //    {
-        //        return RedirectToAction("ExternalLoginFailure");
-        //    }
+        // GET: /Account/ExternalLoginCallback
+        [AllowAnonymous]
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            AuthenticationResult result = OAuthWebSecurity.VerifyAuthentication(Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
+            if (!result.IsSuccessful)
+            {
+                return RedirectToAction("ExternalLoginFailure");
+            }
 
-        //    if (OAuthWebSecurity.Login(result.Provider, result.ProviderUserId, createPersistentCookie: false))
-        //    {
-        //        return RedirectToLocal(returnUrl);
-        //    }
+            if (OAuthWebSecurity.Login(result.Provider, result.ProviderUserId, createPersistentCookie: false))
+            {
+                return RedirectToLocal(returnUrl);
+            }
 
-        //    if (User.Identity.IsAuthenticated)
-        //    {
-        //        // If the current user is logged in add the new account
-        //        OAuthWebSecurity.CreateOrUpdateAccount(result.Provider, result.ProviderUserId, User.Identity.Name);
-        //        return RedirectToLocal(returnUrl);
-        //    }
-        //    else
-        //    {
-        //        // User is new, ask for their desired membership name
-        //        string loginData = OAuthWebSecurity.SerializeProviderUserId(result.Provider, result.ProviderUserId);
-        //        ViewBag.ProviderDisplayName = OAuthWebSecurity.GetOAuthClientData(result.Provider).DisplayName;
-        //        ViewBag.ReturnUrl = returnUrl;
-        //        return View("ExternalLoginConfirmation", new RegisterExternalLoginModel { UserName = result.UserName, ExternalLoginData = loginData });
-        //    }
-        //}
+            if (User.Identity.IsAuthenticated)
+            {
+                // If the current user is logged in add the new account
+                OAuthWebSecurity.CreateOrUpdateAccount(result.Provider, result.ProviderUserId, User.Identity.Name);
+                return RedirectToLocal(returnUrl);
+            }
+            else
+            {
+                // User is new, ask for their desired membership name
+                string loginData = OAuthWebSecurity.SerializeProviderUserId(result.Provider, result.ProviderUserId);
+                ViewBag.ProviderDisplayName = OAuthWebSecurity.GetOAuthClientData(result.Provider).DisplayName;
+                ViewBag.ReturnUrl = returnUrl;
+                return View("ExternalLoginConfirmation", new RegisterExternalLoginModel { UserName = result.UserName, ExternalLoginData = loginData });
+            }
+        }
 
-        //// POST: /Account/ExternalLoginConfirmation
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult ExternalLoginConfirmation(RegisterExternalLoginModel model, string returnUrl)
-        //{
-        //    string provider = null;
-        //    string providerUserId = null;
+        // POST: /Account/ExternalLoginConfirmation
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExternalLoginConfirmation(RegisterExternalLoginModel model, string returnUrl)
+        {
+            string provider = null;
+            string providerUserId = null;
 
-        //    if (User.Identity.IsAuthenticated || !OAuthWebSecurity.TryDeserializeProviderUserId(model.ExternalLoginData, out provider, out providerUserId))
-        //    {
-        //        return RedirectToAction("Manage");
-        //    }
+            if (User.Identity.IsAuthenticated || !OAuthWebSecurity.TryDeserializeProviderUserId(model.ExternalLoginData, out provider, out providerUserId))
+            {
+                return RedirectToAction("Manage");
+            }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        // Insert a new user into the database
-        //        using (UsersContext db = new UsersContext())
-        //        {
-        //            UserProfile user = db.UserProfiles.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
+            if (ModelState.IsValid)
+            {
+                // Insert a new user into the database
+                using (UsersContext db = new UsersContext())
+                {
+                    UserProfile user = db.UserProfiles.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
 
-        //            // Check if user already exists
-        //            if (user == null)
-        //            {
-        //                // Insert name into the profile table
-        //                db.UserProfiles.Add(new UserProfile { UserName = model.UserName });
-        //                db.SaveChanges();
+                    // Check if user already exists
+                    if (user == null)
+                    {
+                        // Insert name into the profile table
+                        db.UserProfiles.Add(new UserProfile { UserName = model.UserName });
+                        db.SaveChanges();
 
-        //                OAuthWebSecurity.CreateOrUpdateAccount(provider, providerUserId, model.UserName);
-        //                OAuthWebSecurity.Login(provider, providerUserId, createPersistentCookie: false);
+                        OAuthWebSecurity.CreateOrUpdateAccount(provider, providerUserId, model.UserName);
+                        OAuthWebSecurity.Login(provider, providerUserId, createPersistentCookie: false);
 
-        //                return RedirectToLocal(returnUrl);
-        //            }
-        //            else
-        //            {
-        //                ModelState.AddModelError("UserName", "User name already exists. Please enter a different user name.");
-        //            }
-        //        }
-        //    }
+                        return RedirectToLocal(returnUrl);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("UserName", "User name already exists. Please enter a different user name.");
+                    }
+                }
+            }
 
-        //    ViewBag.ProviderDisplayName = OAuthWebSecurity.GetOAuthClientData(provider).DisplayName;
-        //    ViewBag.ReturnUrl = returnUrl;
-        //    return View(model);
-        //}
+            ViewBag.ProviderDisplayName = OAuthWebSecurity.GetOAuthClientData(provider).DisplayName;
+            ViewBag.ReturnUrl = returnUrl;
+            return View(model);
+        }
 
-        //// GET: /Account/ExternalLoginFailure
-        //[AllowAnonymous]
-        //public ActionResult ExternalLoginFailure()
-        //{
-        //    return View();
-        //}
+        // GET: /Account/ExternalLoginFailure
+        [AllowAnonymous]
+        public ActionResult ExternalLoginFailure()
+        {
+            return View();
+        }
 
-        //[AllowAnonymous]
-        //[ChildActionOnly]
-        //public ActionResult ExternalLoginsList(string returnUrl)
-        //{
-        //    ViewBag.ReturnUrl = returnUrl;
-        //    return PartialView("_ExternalLoginsListPartial", OAuthWebSecurity.RegisteredClientData);
-        //}
+        [AllowAnonymous]
+        [ChildActionOnly]
+        public ActionResult ExternalLoginsList(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return PartialView("_ExternalLoginsListPartial", OAuthWebSecurity.RegisteredClientData);
+        }
 
-        //[ChildActionOnly]
-        //public ActionResult RemoveExternalLogins()
-        //{
-        //    ICollection<OAuthAccount> accounts = OAuthWebSecurity.GetAccountsFromUserName(User.Identity.Name);
-        //    List<ExternalLogin> externalLogins = new List<ExternalLogin>();
-        //    foreach (OAuthAccount account in accounts)
-        //    {
-        //        AuthenticationClientData clientData = OAuthWebSecurity.GetOAuthClientData(account.Provider);
+        [ChildActionOnly]
+        public ActionResult RemoveExternalLogins()
+        {
+            ICollection<OAuthAccount> accounts = OAuthWebSecurity.GetAccountsFromUserName(User.Identity.Name);
+            List<ExternalLogin> externalLogins = new List<ExternalLogin>();
+            foreach (OAuthAccount account in accounts)
+            {
+                AuthenticationClientData clientData = OAuthWebSecurity.GetOAuthClientData(account.Provider);
 
-        //        externalLogins.Add(new ExternalLogin
-        //        {
-        //            Provider = account.Provider,
-        //            ProviderDisplayName = clientData.DisplayName,
-        //            ProviderUserId = account.ProviderUserId,
-        //        });
-        //    }
+                externalLogins.Add(new ExternalLogin
+                {
+                    Provider = account.Provider,
+                    ProviderDisplayName = clientData.DisplayName,
+                    ProviderUserId = account.ProviderUserId,
+                });
+            }
 
-        //    ViewBag.ShowRemoveButton = externalLogins.Count > 1 || OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
-        //    return PartialView("_RemoveExternalLoginsPartial", externalLogins);
-        //}
+            ViewBag.ShowRemoveButton = externalLogins.Count > 1 || OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
+            return PartialView("_RemoveExternalLoginsPartial", externalLogins);
+        }
 
         #region Helpers
         private ActionResult RedirectToLocal(string returnUrl)
@@ -324,23 +325,23 @@ namespace EuroManager.WorldSimulator.Presentation.Web.Controllers
             }
         }
 
-        //internal class ExternalLoginResult : ActionResult
-        //{
-        //    public ExternalLoginResult(string provider, string returnUrl)
-        //    {
-        //        Provider = provider;
-        //        ReturnUrl = returnUrl;
-        //    }
+        internal class ExternalLoginResult : ActionResult
+        {
+            public ExternalLoginResult(string provider, string returnUrl)
+            {
+                Provider = provider;
+                ReturnUrl = returnUrl;
+            }
 
-        //    public string Provider { get; private set; }
+            public string Provider { get; private set; }
 
-        //    public string ReturnUrl { get; private set; }
+            public string ReturnUrl { get; private set; }
 
-        //    public override void ExecuteResult(ControllerContext context)
-        //    {
-        //        OAuthWebSecurity.RequestAuthentication(Provider, ReturnUrl);
-        //    }
-        //}
+            public override void ExecuteResult(ControllerContext context)
+            {
+                OAuthWebSecurity.RequestAuthentication(Provider, ReturnUrl);
+            }
+        }
 
         private static string ErrorCodeToString(MembershipCreateStatus createStatus)
         {
