e845c20e8855a4d26ae832c28bf258162bec267e
Rafal Barszczewski <rafalb>
2013-04-06 18:28:24 +0200

    Main layout adjustments, smart navigation links

---
+++ Source/EuroManager.WorldSimulator.Presentation.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace EuroManager.WorldSimulator.Presentation.Web.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
+        {
+            var tag = new TagBuilder("li");
+            tag.InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString();
+
+            string currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+
+            if (String.Equals(actionName, currentAction, StringComparison.InvariantCultureIgnoreCase) &&
+                String.Equals(controllerName, currentController, StringComparison.InvariantCultureIgnoreCase))
+            {
+                tag.AddCssClass("active");
+            }
+
+            string html = tag.ToString();
+            return new MvcHtmlString(html);
+        }
+    }
+}
